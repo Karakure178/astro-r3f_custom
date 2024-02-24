2d521398f5fa21349e4df946ba1a391145184f8d
Karakure178 <egg9017shun@gmail.com>
2024-02-24 11:23:38 +0900

    add - 円柱をくり抜いたオブジェクト追加

---
+++ src/components/r3f/obj1/App2.tsx
@@ -0,0 +1,65 @@
+// 実装参考
+// https://sbcode.net/react-three-fiber/use-loader/
+/**
+ * 裏面の描画方法：
+ * https://qiita.com/edo_m18/items/8b84e6318ee49713e40e
+ */
+
+import { Vector2, ShaderMaterial, TextureLoader, DoubleSide } from "three";
+import { useRef, useEffect } from "react";
+import { useFrame, Canvas, useThree, useLoader } from "@react-three/fiber";
+import { CameraControls } from "@react-three/drei";
+import fs from "./normal.frag";
+import vx from "./normal.vert";
+import "./Fragments.scss";
+
+const Scene = () => {
+  const { controls } = useThree();
+  const meshRef = useRef();
+
+  // 以降テクスチャ絡みの設定
+  const fragmentShader = fs;
+  const vertexShader = vx;
+  const img = "public/assets/img/f_triangle.png";
+  const tex = useLoader(TextureLoader, img);
+
+  const material = new ShaderMaterial({
+    vertexShader: vertexShader,
+    fragmentShader: fragmentShader,
+    uniforms: {
+      u_time: { type: "f", value: 1.0 },
+      u_resolution: { type: "v2", value: new Vector2() },
+      u_mouse: { type: "v2", value: new Vector2() },
+      u_texture: { type: "t", value: tex },
+    },
+  });
+  material.side = DoubleSide;
+
+  useEffect(() => {
+    if (controls) {
+      controls.rotateTo(Math.PI / -4, Math.PI / 2.5, true);
+    }
+  }, [controls]);
+
+  useFrame(({ clock }) => {
+    meshRef.current.material.uniforms.u_time.value = clock.elapsedTime;
+  });
+  return (
+    <>
+      <mesh ref={meshRef} material={material}>
+        <boxGeometry args={[1, 1, 1]} />
+      </mesh>
+      <CameraControls makeDefault />
+    </>
+  );
+};
+
+export default function App() {
+  return (
+    <div className="container">
+      <Canvas camera={{ position: [0, 0, 3], fov: 100 }}>
+        <Scene />
+      </Canvas>
+    </div>
+  );
+}
